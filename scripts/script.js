700b4d9b5aeb344df6af21734bee66fc493651c4
Cauã Raphael <dev.cauaraphael@gmail.com>
2025-02-25 00:06:21 -0300

    inicio do desenvolvimento do exercicio

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+function calculoIMC() {
+
+    const peso = parseFloat(document.getElementById('peso')).value;
+    const altura = parseFloat(document.getElementById('altura')).value;
+    const sexo = document.getElementById('sexo').value;
+
+    if(sexo == 'masculino') {
+        
+
+}
